4ebcb77d70be038c306b7cf54dcbda290e81098a
Alexandru Toderică <a.toderica.2013@gmail.com>
2018-04-01 21:59:09 +0300

    schimbare date conectare db

--- assets/php/config.php
+++ assets/php/config.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'allergyhelp');
+define('DB_SERVER', 'sql11.freesqldatabase.com');
+define('DB_USERNAME', 'sql11230142');
+define('DB_PASSWORD', '6TdFtjlMZ9');
+define('DB_DATABASE', 'sql11230142');
 ?>
