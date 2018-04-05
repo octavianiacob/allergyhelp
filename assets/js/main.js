b915f70c4ed86e906296c476ca270ad958eafdfe
Octavian Iacob <octavzz1999@gmail.com>
2018-04-05 13:34:07 +0300

    contact section & carousel text

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function ($) {
 
   // Fixed Header
   $(window).scroll(function () {
-    if ($(this).scrollTop() > 800) {
+    if ($(this).scrollTop() > 100) {
       $('#header').addClass('header-fixed');
     } else {
       $('#header').removeClass('header-fixed');
