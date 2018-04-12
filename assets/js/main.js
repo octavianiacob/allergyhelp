6e3911c7b183f206e205b517651529ee46535e86
Alexandru Toderică <a.toderica.2013@gmail.com>
2018-04-12 11:05:46 +0300

    schimbat navbar in landing page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,32 @@
-jQuery(document).ready(function ($) {
-
-  // Fixed Header
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {
-      $('#header').addClass('header-fixed');
-    } else {
-      $('#header').removeClass('header-fixed');
-    }
-  });
+$(document).ready(function()
+{
+	$(window).scroll(function() {
+		if($(window).width() > 991)
+		{
+			if ($(window).scrollTop() > 0)
+			{
+				$(".navbar").addClass("bg-dark");
+				$(".navbar").css("box-shadow", "0 2px 2px 0 rgba(0, 0, 0, 0.07)");
+				$(".navbar-nav").addClass("ml-auto");
+				$(".navbar-nav").removeClass("m-auto");
+				$(".navbar-brand").removeClass("d-none");
+			}
+			else
+			{
+				$(".navbar").removeClass("bg-dark");
+				$(".navbar").css("box-shadow", "none");
+				$(".navbar-nav").removeClass("ml-auto");
+				$(".navbar-nav").addClass("m-auto");
+				$(".navbar-brand").addClass("d-none");
+			}
+		}
+	});
+	if($(window).width() <= 991)
+	{
+		$(".navbar").addClass("bg-dark");
+		$(".navbar").css("box-shadow", "0 2px 2px 0 rgba(0, 0, 0, 0.07)");
+		$(".navbar-nav").addClass("ml-auto");
+		$(".navbar-nav").removeClass("m-auto");
+		$(".navbar-brand").removeClass("d-none");
+	}
 });
