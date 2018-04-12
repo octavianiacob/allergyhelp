83c85577f4b8109eaa5fb3b7dbc13e0032c871b7
Octavian Iacob <octavzz1999@gmail.com>
2018-04-12 12:13:39 +0300

    redesign

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,17 +7,11 @@ $(document).ready(function()
 			{
 				$(".navbar").addClass("bg-dark");
 				$(".navbar").css("box-shadow", "0 2px 2px 0 rgba(0, 0, 0, 0.07)");
-				$(".navbar-nav").addClass("ml-auto");
-				$(".navbar-nav").removeClass("m-auto");
-				$(".navbar-brand").removeClass("d-none");
 			}
 			else
 			{
 				$(".navbar").removeClass("bg-dark");
 				$(".navbar").css("box-shadow", "none");
-				$(".navbar-nav").removeClass("ml-auto");
-				$(".navbar-nav").addClass("m-auto");
-				$(".navbar-brand").addClass("d-none");
 			}
 		}
 	});
@@ -25,8 +19,5 @@ $(document).ready(function()
 	{
 		$(".navbar").addClass("bg-dark");
 		$(".navbar").css("box-shadow", "0 2px 2px 0 rgba(0, 0, 0, 0.07)");
-		$(".navbar-nav").addClass("ml-auto");
-		$(".navbar-nav").removeClass("m-auto");
-		$(".navbar-brand").removeClass("d-none");
 	}
 });
